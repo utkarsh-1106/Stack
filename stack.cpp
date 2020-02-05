36c11c381d67353a705d461ab39aeb008bf1938d
Utkarsh Sinha <43957190+utkarsh-1106@users.noreply.github.com>
2020-02-05 23:08:24 +0530

    Adding stack implementation using Linked-List

--- stack.cpp
+++ stack.cpp
@@ -1,3 +1,5 @@
+// Stack using Array
+
 #include<bits/stdc++.h>
 using namespace std;
 struct Stack{
@@ -92,6 +94,117 @@ int main(){
 	    	    break;
     	    case 5:
 	    	    cout<<"\nNumber of element present in the stack is: "<<newStack.numOfElement;
+	    	    break;
+    	    default:
+	    	    cout<<"\nEnter valid choice\n";
+	    	    break;
+	    }
+	    cout<<"\nWant to continue press '1' else press '0': ";
+	    cin>>flag;
+	}
+	return 0;
+}
+
+// Stack using Linked-List
+
+#include<bits/stdc++.h>
+using namespace std;
+struct stackNode{
+	int data;
+	struct stackNode *next;
+}*Top = NULL;
+int numOfElement = 0;
+bool isEmpty(){
+	return (Top == NULL) ? 1 : 0;
+}
+bool isFull(){
+	struct stackNode *newNode = new stackNode;
+	int full = (newNode == NULL) ? 1 : 0;
+	return full;
+}
+void push(int value){
+	cout<<endl;
+	struct stackNode *newNode = new stackNode;
+	if(newNode == NULL){
+		cout<<"\nStack is Full";
+	} else {
+		newNode->data = value;
+		newNode->next = Top;
+		Top = newNode;
+		numOfElement++;
+	}
+}
+int pop(){
+	cout<<endl;
+	if(isEmpty()){
+		cout<<"\nStack is Empty";
+		return INT_MIN;
+	} else {
+		struct stackNode *temp = Top;
+		int value = Top->data;
+		Top = Top->next;
+		numOfElement--;
+		delete temp;
+		return value;
+	}
+}
+int peek(int pos){
+	cout<<endl;
+	if(isEmpty()){
+		cout<<"\nStack is Empty";
+		return INT_MIN;
+	} else if(pos>0 && pos<=numOfElement){
+		struct stackNode *p = Top;
+		while(--pos){
+			p = p->next;
+		}
+		return p->data;
+	} else {
+		cout<<"Invalid position\n";
+		return INT_MIN;
+	}
+}
+void display(){
+	cout<<endl;
+	struct stackNode *temp = Top;
+	cout<<"Displaying Stack:\n";
+	cout<<"Top\n";
+	while(temp){
+		cout<<"----\n";
+		cout<<"|"<<temp->data<<"|"<<endl;
+		temp = temp->next;
+	}cout<<"----\n";
+}
+int main(){
+	int choice, flag = 1, pos;
+	cout<<"1. For pushing element in stack press '1'\n";
+	cout<<"2. For poping out element in stack press '2'\n";
+	cout<<"3. For peeking the stack press '3'\n";
+	cout<<"4. For displaying stack element press '4'\n";
+	cout<<"5. For displaying number of element present in the stack press '5'\n";
+	while(flag != 0){
+	    cout<<"\nEnter choice '1', '2', '3', '4' or '5': ";
+    	cin>>choice;
+    	switch(choice){
+    	    case 1:
+	    	    cout<<"\nEnter value to push: ";
+	    	    int value;
+	    	    cin>>value;
+	    	    push(value);
+	    	    break;
+    	    case 2:
+	    	    cout<<"\nPopped item: "<<pop();
+	    	    break;
+    	    case 3:
+	    	    cout<<"\nEnter the position of item you want to peek: ";
+	    	    cin>>pos;
+	    	    cout<<"\nThe "<<pos<<"th element is: "<<peek(pos);
+	    	    break;
+    	    case 4:
+	    	    display();
+	    	    break;
+    	    case 5:
+	    	    cout<<"\nNumber of element present in the stack is: "<<numOfElement;
 	    	    break;
     	    default:
 	    	    cout<<"\nEnter valid choice\n";
